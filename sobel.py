72a5680a7bbd674dae512477ea529f5947eed14a
Hieu-Git07 <mysmine2007@gmail.com>
2025-11-27 13:43:23 +0700

    Update sobel.py

--- sobel.py
+++ sobel.py
@@ -1,16 +1,17 @@
 import numpy as np
-import sympy as sp
 from PIL import Image
 import matplotlib.pyplot as plt
 import math
-import time
+
+# Author: ------------NguyenDinhTrongHieu--------------
+##   /\_/\  
+##  ( o.o ) 
+##   > ^ < 
 
 
 # Convert image to 2D vector (grayscale)
-img = Image.open("parrot(gs).png").convert("L")
+img = Image.open("leaf(rgb).png").convert("L")
 img = np.array(img)
-print(img)
-print(img.shape)
 
 kernel_x = [
     [1, 0, -1],
@@ -48,43 +49,57 @@ for i in range(1, img_pad.shape[0]-1):
         grad_val[i-1][j-1] = math.sqrt(grad_x[i-1][j-1]**2 + grad_y[i-1][j-1]**2)
         max_val = max(max_val,grad_val[i - 1][j - 1])
         
-
-for i in range(1, img_pad.shape[0]-1):
-    for j in range(1, img_pad.shape[1] - 1):
-        # Xac xuat = do thay doi / do thay doi max (for soft_edge)
-        prob[i-1][j-1] = grad_val[i-1][j-1]/max_val
-
-# Hard Threshold (Goi la Hard Threshold la vi se so sanh voi Threshold va set 0 / 255)
+# Hard Threshold 
 threshold = 0
 for i in range(img.shape[0] - 1):
     for j in range(img.shape[1] - 1):
-        threshold += grad_val[i - 1][j - 1]
+        threshold += grad_val[i][j]
         
-threshold /= (img.shape[0] - 1) * (img.shape[1] - 1)
+threshold /= (img.shape[0]) * (img.shape[1])
 
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
-        if grad_val[i][j] > threshold:
-            new_edge_image_threshold[i][j] = 255
-        else:
+        if grad_val[i][j] < threshold:
             new_edge_image_threshold[i][j] = 0
+        else:
+            new_edge_image_threshold[i][j] = grad_val[i][j]
 
 
-# Soft edge
+# Tim kiem max val de xu ly min-max scaling
+for i in range(1, img_pad.shape[0]-1):
+    for j in range(1, img_pad.shape[1] - 1):
+        prob[i-1][j-1] = grad_val[i-1][j-1]/max_val
+
+
+# Min-max scaling
 for i in range(img.shape[0]):
     for j in range(img.shape[1]):
         new_edge_image[i][j] = prob[i][j] * 255
 
-# Boosting edge for soft edge
-for i in range(img.shape[0]):
-    for j in range(img.shape[1]):
-        new_edge_image[i][j] = min(255, new_edge_image[i][j] * 1.25)
 
-# Plot anh cuc ki gay (co the plot thu threshold de so sanh)
 
-plt.imshow(new_edge_image, cmap = 'gray')
+plt.figure(figsize=(12, 4))
+
+# Image 1
+plt.subplot(1, 3, 1)
+plt.title("Gradient Value")
+plt.imshow(grad_val, cmap='gray')
+plt.axis('off')
+
+# Image 2
+plt.subplot(1, 3, 2)
+plt.title("Min-max scaling")
+plt.imshow(new_edge_image, cmap='gray')
+plt.axis('off')
+
+# Image 3
+plt.subplot(1, 3, 3)
+plt.title("Hardthreshold")
+plt.imshow(new_edge_image_threshold, cmap='gray')
+plt.axis('off')
+
+plt.tight_layout()
 plt.show()
-
 
 
 
